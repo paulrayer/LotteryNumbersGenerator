e636bc21164060ad1a2fd59ddc0d9b1fbc9099ea
paulr <paul.rayer@hotmail.com>
2021-10-24 23:29:58 +0100

    Added removed projects Added tests and tested Jenkins build ( build failed if any test failed)

---
+++ RandomNumbersGenerator/LotteryNumbersGenerator.Tests/MSTestSample.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryNumbersGenerator.Tests
+{
+    [TestClass]
+    public class MSTestSample
+    {
+        [Ignore]
+        [TestMethod]
+        public void TestOne()
+        {
+            Assert.IsTrue(1 == 0);
+        }
+    }
+}
